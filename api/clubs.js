a0334e157d2dec96167cda659bcb252f2ea0e6f9
schwiddi <schwiddi@me.com>
2019-01-29 19:16:53 +0100

    switched to db.querry

--- api/clubs.js
+++ api/clubs.js
@@ -11,17 +11,12 @@ function validateNewClub(data) {
 }
 
 clubs.get('/', (req, res) => {
-  db.getConnection()
-    .then(conn => {
-      const result = conn.query('SELECT * FROM t_clubs;');
-      conn.release();
-      return result;
-    })
-    .then(result => {
-      if (isEmpty(result[0])) {
+  db.query('SELECT * FROM t_clubs;')
+    .then(rows => {
+      if (isEmpty(rows[0])) {
         res.sendStatus(204);
       } else {
-        res.status(200).send(result[0]);
+        res.status(200).send(rows[0]);
       }
     })
     .catch(err => {
@@ -31,17 +26,12 @@ clubs.get('/', (req, res) => {
 });
 
 clubs.get('/:c_id', (req, res) => {
-  db.getConnection()
-    .then(conn => {
-      const result = conn.query(`SELECT c_id, c_name, c_description, c_initiator, c_ts_insert, c_ts_update FROM t_clubs WHERE c_id = ${req.params.c_id}`);
-      conn.release();
-      return result;
-    })
-    .then(result => {
-      if (isEmpty(result[0])) {
+  db.query(`SELECT c_id, c_name, c_description, c_initiator, c_ts_insert, c_ts_update FROM t_clubs WHERE c_id = ${req.params.c_id}`)
+    .then(rows => {
+      if (isEmpty(rows[0])) {
         res.sendStatus(204);
       } else {
-        res.status(200).send(result[0]);
+        res.status(200).send(rows[0][0]);
       }
     })
     .catch(err => {
@@ -55,30 +45,16 @@ clubs.post('/', (req, res) => {
   if (tmp.error) {
     res.status(406).send(`${tmp.error.name}: ${tmp.error.details[0].message}`);
   } else {
-    db.getConnection()
-      .then(conn => {
-        const result = conn.query(`INSERT INTO t_clubs (c_name, c_description, c_initiator) VALUES('${req.body.c_name}', '${req.body.c_description}', '${req.body.c_iniator}');`);
-        conn.release();
-        return result;
+    db.query(`INSERT INTO t_clubs (c_name, c_description, c_initiator) VALUES('${req.body.c_name}', '${req.body.c_description}', '${req.body.c_iniator}');`)
+      .then(rows => {
+        return db.query(`SELECT c_id, c_name, c_description, c_initiator, c_ts_insert FROM t_clubs WHERE c_id = ${rows[0].insertId}`);
       })
-      .then(result => {
-        db.getConnection()
-          .then(conn => {
-            const ret = conn.query(`SELECT c_id, c_name, c_description, c_initiator, c_ts_insert FROM t_clubs WHERE c_id = ${result[0].insertId}`);
-            conn.release();
-            return ret;
-          })
-          .then(ret => {
-            if (isEmpty(ret[0])) {
-              res.sendStatus(500);
-            } else {
-              res.status(201).send(ret[0]);
-            }
-          })
-          .catch(err => {
-            res.sendStatus(500);
-            log.error(err.message);
-          });
+      .then(rows => {
+        if (isEmpty(rows[0])) {
+          res.sendStatus(500);
+        } else {
+          res.status(201).send(rows[0][0]);
+        }
       })
       .catch(err => {
         res.sendStatus(500);
