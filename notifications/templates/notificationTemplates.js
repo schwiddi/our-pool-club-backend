cc3a307bdc7d3fe97c13f8a485e9a7a594a6621b
schwiddi <schwiddi@me.com>
2019-01-29 23:31:06 +0100

    made new env var for the url

--- notifications/templates/notificationTemplates.js
+++ notifications/templates/notificationTemplates.js
@@ -3,14 +3,14 @@ function completeRegistration(receiver, username, regkey) {
     from: '"Pool Club" <r21billard@gmail.com>',
     to: receiver,
     subject: 'Welcome & Complete Registration',
-    text: `Activate Account Link: http://${process.env.BACKEND_DB_HOST}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}`,
+    text: `Activate Account Link: ${process.env.BACKEND_HTTP_URL}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}`,
     html: `
       <h3>Hi ${username},</h3>
       <p>this here is the activation mail for your account that you've just created.</p>
       <p></p>
       <p>Please use the following Link to complete the registration Process.</p>
       <p></p>
-      <p>Link: http://${process.env.BACKEND_DB_HOST}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}</p>
+      <p>${process.env.BACKEND_HTTP_URL}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}</p>
       <p></p>
       <p>Cheers</p>
       <p>your Pool Club Team</p>`,
