0fabf189f59167834f0a23b734b3273e11b59fcb
schwiddi <schwiddi@me.com>
2019-01-29 19:18:24 +0100

    query not get conn

--- common/reqLogger.js
+++ common/reqLogger.js
@@ -4,11 +4,9 @@ const db = require('../db/db_connection');
 async function reqLogger(req) {
   const reqString = `req.hostname=${req.hostname} req.ip=${req.ip} req.method=${req.method} req.originalUrl=${req.originalUrl} req.path=${req.path} req.protocol=${req.protocol}`;
   log.info(`Request Logger: ${reqString}`);
-  db.getConnection()
-    .then(conn => {
-      const result = conn.query(`INSERT INTO inf_req_log (req) VALUES ('${reqString}');`);
-      conn.release();
-      return result;
+  db.query(`INSERT INTO inf_req_log (req) VALUES ('${reqString}');`)
+    .then(() => {
+      return;
     })
     .catch(err => {
       log.error(err.message);
