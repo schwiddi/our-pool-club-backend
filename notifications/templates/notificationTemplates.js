c1f3ebe840a2dd2bdd2e96cdbe8b30bec4f3d01f
schwiddi <schwiddi@me.com>
2019-02-04 18:10:06 +0100

    refactor

--- notifications/templates/notificationTemplates.js
+++ notifications/templates/notificationTemplates.js
@@ -1,21 +1,26 @@
 function completeRegistration(receiver, username, regkey) {
+  const url = `${process.env.BACKEND_HTTP_URL}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}`;
   return {
     from: '"Pool Club" <r21billard@gmail.com>',
     to: receiver,
     subject: 'Welcome & Complete Registration',
-    text: `Activate Account Link: ${process.env.BACKEND_HTTP_URL}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}`,
+    text: `Activate Account Link: ${url}`,
     html: `
-      <h3>Hi ${username},</h3>
-      <p>this here is the activation mail for your account that you've just created.</p>
-      <br>
-      <p>Please use the following Link to complete the registration Process.</p>
-      <br>
-      <p>${process.env.BACKEND_HTTP_URL}:${process.env.BACKEND_HTTP_PORT}/api/v1/users/completeRegistration/${regkey}</p>
-      <br>
-      <br>
-      <p>Cheers<br>
-      your Pool Club Team</p>
-      `,
+            <h3>
+              Hi ${username},
+            </h3>
+            <p>
+              this here is the activation mail for your account that you've just created.
+            </p>
+            <p>
+              Please use the following Link to complete the registration Process:<br>
+              ${url}
+            </p>
+            <p>
+              Cheers<br>
+              your Pool Club Team
+            </p>
+          `,
   };
 }
 
