d1c2d9f517157430971c0d13f2d2c49258205041
schwiddi <schwiddi@me.com>
2019-02-04 17:52:13 +0100

    refactor to db.querry

--- api/users.js
+++ api/users.js
@@ -8,23 +8,13 @@ const { new_user, } = require('../schemas/joiInVal');
 const users = express.Router();
 const md5 = require('md5');
 
-function validateNewUser(data) {
-  return Joi.validate(data, new_user);
-}
-
-// later you should protect this route that only admins can get that
 users.get('/', (req, res) => {
-  db.getConnection()
-    .then(conn => {
-      const result = conn.query('SELECT u_id, u_name, u_mail, u_ts_insert, u_ts_update FROM t_users');
-      conn.release();
-      return result;
-    })
-    .then(result => {
-      if (isEmpty(result[0])) {
+  db.query('SELECT u_id, u_name, u_mail, u_ts_insert, u_ts_update FROM t_users')
+    .then(rows => {
+      if (isEmpty(rows[0])) {
         res.sendStatus(204);
       } else {
-        res.status(200).send(result[0]);
+        res.status(200).send(rows[0]);
       }
     })
     .catch(err => {
@@ -34,17 +24,12 @@ users.get('/', (req, res) => {
 });
 
 users.get('/:u_id', (req, res) => {
-  db.getConnection()
-    .then(conn => {
-      const result = conn.query(`SELECT u_id, u_name, u_mail, u_ts_insert, u_ts_update FROM t_users WHERE u_id = ${req.params.u_id}`);
-      conn.release();
-      return result;
-    })
-    .then(result => {
-      if (isEmpty(result[0])) {
+  db.query(`SELECT u_id, u_name, u_mail, u_ts_insert, u_ts_update FROM t_users WHERE u_id = ${req.params.u_id}`)
+    .then(rows => {
+      if (isEmpty(rows[0])) {
         res.sendStatus(204);
       } else {
-        res.status(200).send(result[0][0]);
+        res.status(200).send(rows[0][0]);
       }
     })
     .catch(err => {
@@ -54,48 +39,40 @@ users.get('/:u_id', (req, res) => {
 });
 
 users.post('/', (req, res) => {
-  const tmp = validateNewUser(req.body);
-  if (tmp.error) {
-    res.status(406).send(`${tmp.error.name}: ${tmp.error.details[0].message} `);
-  } else {
-    const registerKey = md5(`${req.body.u_mail}${req.body.u_name}${new Date()}`);
-    bcrypt.hash(req.body.u_password, 10)
-      .then(hashedpw => {
-        db.getConnection()
-          .then(conn => {
-            const result = conn.query(`INSERT INTO t_users (u_name, u_mail, u_password, u_registration_key) VALUES('${req.body.u_name}', '${req.body.u_mail}', '${hashedpw}', '${registerKey}');`);
-            conn.release();
-            return result;
-          })
-          .then(result => {
-            db.getConnection()
-              .then(conn => {
-                const ret = conn.query(`SELECT u_id, u_name, u_mail, u_ts_insert FROM t_users WHERE u_id = ${result[0].insertId}`);
-                conn.release();
-                return ret;
-              })
-              .then(ret => {
-                if (isEmpty(ret[0])) {
+  Joi.validate(req.body, new_user)
+    .then(() => {
+      const registerKey = md5(`${req.body.u_mail}${req.body.u_name}${new Date()}`);
+      bcrypt.hash(req.body.u_password, 10)
+        .then(hashedpw => {
+          db.query(`INSERT INTO t_users (u_name, u_mail, u_password, u_registration_key) VALUES('${req.body.u_name}', '${req.body.u_mail}', '${hashedpw}', '${registerKey}');`)
+            .then(rows => {
+              db.query(`SELECT u_id, u_name, u_mail, u_ts_insert FROM t_users WHERE u_id = ${rows[0].insertId}`)
+                .then(rows => {
+                  if (isEmpty(rows[0])) {
+                    res.sendStatus(500);
+                  } else {
+                    res.status(201).send(rows[0][0]);
+                  }
+                })
+                .catch(err => {
                   res.sendStatus(500);
-                } else {
-                  res.status(201).send(ret[0][0]);
-                }
-              })
-              .catch(err => {
-                res.sendStatus(500);
-                log.error(err.message);
-              });
-          })
-          .catch(err => {
-            res.sendStatus(500);
-            log.error(err.message);
-          });
-      })
-      .catch(err => {
-        res.sendStatus(500);
-        log.error(err.message);
-      });
-  }
+                  log.error(`db: ${err.message}`);
+                });
+            })
+            .catch(err => {
+              res.sendStatus(500);
+              log.error(`db: ${err.message}`);
+            });
+        })
+        .catch(err => {
+          res.sendStatus(500);
+          log.error(`bcrypt: ${err.message}`);
+        });
+    })
+    .catch(err => {
+      res.status(400).send(`${err.name}: ${err.details[0].message} `);
+      log.warn(`Joi: ${err.message}`);
+    });
 });
 
 users.get('/completeRegistration/:regKey', (req, res) => {
